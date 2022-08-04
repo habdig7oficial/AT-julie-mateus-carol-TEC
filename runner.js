038398c521fd77b8404754f2ba2e57f6b5ce456a
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2022-08-04 12:53:51 -0300

    preset

---
+++ runner.js
@@ -0,0 +1,24 @@
+let dotenv = require("dotenv").config()
+
+let lib_server = require("qse-x")
+
+let lib_db = require("qmm-connect")
+
+
+let server = new lib_server.default({
+    parse: false,
+    port : process.env.PORT,
+    consign: "./routes",
+    assets : "./assets",
+    views : true
+}).start()
+
+
+let db = new lib_db.default().atlas({
+    user: process.env.USERAT,
+    password: process.env.PASSWORDDB,
+    thecluster: process.env.CLUSTER,
+    banco: process.env.BANCO
+})
+
+console.log(process.env.PORT)
