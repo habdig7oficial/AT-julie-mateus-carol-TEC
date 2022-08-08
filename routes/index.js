61c7e8523f2d8da078b9a0f326f0529291f13d60
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2022-08-08 12:24:56 -0300

    auto-deploy

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 module.exports = function ( app ) {
     app.get("/", function (req, res) {
-        res.render("index.ejs", {})
+        res.render("index.ejs", {n_cadastro:222,n_vencidos:1})
     })
 }
