94b2604fadd12d273766656ffee271a442630a89
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2022-08-10 12:56:25 -0300

    auto-deploy

--- assets/JS/mostrar.js
+++ assets/JS/mostrar.js
@@ -1,18 +1,81 @@
 document.addEventListener("DOMContentLoaded", function (params) {
     let validate = document.querySelectorAll(".Validade")
+    let container = document.querySelectorAll(".container-start")
+
+    let click = document.querySelectorAll(".clicking")
 
 
-
-    function select (days){
-        console.log(validate)
+    for (let j = 0; j < click.length; j++) {
 
 
-        for (let i = 0; i < validate.length; i++) {
-            console.log(validate[i].innerHTML)
-            
-        }
+        click[j].addEventListener("click", function (){
+            console.log(validate)
+    
+    
+            let who = click[j].id
+
+            let days 
+
+            console.log(who)
+
+            if (who == "i7") {
+                days = 7
+            }
+
+            else if (who == "i30"){
+                days = 30
+            }
+
+            else{
+                days = 0 
+            }
+
+            //alert(days)
+
+
+            for (let i = 0; i < validate.length; i++) {
+
+
+                let limit = new Date()
+
+                limit.setDate(limit.getDate() + days)
+                
+                limit = Date.parse(limit)
+
+                let Data_validade = Date.parse(validate[i].innerHTML)
+
+                console.log(`id:${who}\ndata:${Data_validade}\nlimite:${limit}`)
+
+                if (days != 0 ) {
+                    if ( Data_validade > limit){
+                    container[i].classList.add("d-none")
+                    alert(true)
+                    }
+                    else{
+                    container[i].classList.remove("d-none")
+                    alert(false)
+                    }
+                }
+                else{
+                    container[i].classList.remove("d-none")
+                }
+
+                
+
+
+                //alert(limit - Date.parse(validate[i].innerHTML))
+                
+            }
+        })
     }
 
-    
+
+
+
+
+console.log(click)
+
+
+
 
 })
