4f4e9803c978209d72fb2b98815827979be3a40f
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2022-08-10 10:01:28 -0300

    auto-deploy

---
+++ assets/JS/mostrar.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function (params) {
+    let validate = document.querySelectorAll(".Validade")
+
+
+
+    function select (days){
+        console.log(validate)
+
+
+        for (let i = 0; i < validate.length; i++) {
+            console.log(validate[i].innerHTML)
+            
+        }
+    }
+
+    
+
+})
